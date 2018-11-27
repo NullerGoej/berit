eb818676d89b9d5c3aed2805dffe623d5fa0fd64
Nico - Legion <swahgnerdk@gmail.com>
2018-11-27 14:53:27 +0100

    Added show tasks

--- website/src/js/index.ts
+++ website/src/js/index.ts
@@ -19,9 +19,42 @@ let Return: HTMLDivElement = <HTMLDivElement>document.getElementById("return");
 let AddTaskB: HTMLButtonElement = <HTMLButtonElement>document.getElementById("taskAdd");
 AddTaskB.addEventListener("click", AddTask);
 
+let ShowTasksB: HTMLButtonElement = <HTMLButtonElement>document.getElementById("showTasks");
+ShowTasksB.addEventListener("click", ShowTasks)
+
 function AddTask(): void {
     let title: string = (<HTMLInputElement>document.getElementById("taskTitle")).value;
     axios.post<Task>(taskUri, {uid: 1, description: title})
         .then((response: AxiosResponse) => { Return.innerHTML = "response " + response.status + " " + response.statusText; })
         .catch((error: AxiosError) => { Return.innerHTML = ""+error; });
+}
+
+function ShowTasks(): void {
+    axios.get<Task[]>(taskUri)
+        .then(function (response: AxiosResponse<Task[]>): void {
+            let result: string = "<table><tr>"+
+            "<th>Tid</th>"+
+            "<th>Uid</th>"+
+            "<th>Timestamp</th>"+
+            "<th>Endstamp</th>"+
+            "<th>Description</th>"+
+            "<th>Done</th>"+
+            "</tr>";
+
+            response.data.forEach((task: Task) => {
+                result += "<tr><td>" + task.tid + "</td>"
+                + "<td>" + task.uid + "</td>"
+                + "<td>" + task.timestamp + "</td>"
+                + "<td>" + task.endstamp + "</td>"
+                + "<td>" + task.description + "</td>"
+                + "<td>" + task.done + "</td></tr>";
+            })
+            result += "</table>";
+
+            Return.innerHTML = result;
+        })
+        .catch(function (error: AxiosError): void {
+            Return.innerHTML = ""+error;
+        })
+      console.log("Done");
 }

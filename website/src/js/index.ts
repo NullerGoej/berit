c32a7860665b3ec26106a4b77c7af096b404f3b6
NullerGoej <bastian2010@live.dk>
2018-11-27 13:39:31 +0100

    userstory #2 done

--- website/src/js/index.ts
+++ website/src/js/index.ts
@@ -1,12 +1,27 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError} 
+    from "../../node_modules/axios/index";
+
+interface Task {
+    tid: number;
+    uid: number;
+    timestamp: Date;
+    endstamp: Date;
+    description: string;
+    done: boolean;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "Bastian", lastName: "Moedekjaer" };
+var taskUri: string = "https://berit.azurewebsites.net/api/task/";
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let Return: HTMLDivElement = <HTMLDivElement>document.getElementById("return");
+
+let AddTaskB: HTMLButtonElement = <HTMLButtonElement>document.getElementById("taskAdd");
+AddTaskB.addEventListener("click", AddTask);
+
+function AddTask(): void {
+    let title: string = (<HTMLInputElement>document.getElementById("taskTitle")).value;
+    axios.post<Task>(taskUri, {uid: 1, description: title})
+        .then((response: AxiosResponse) => { Return.innerHTML = "response " + response.status + " " + response.statusText; })
+        .catch((error: AxiosError) => { Return.innerHTML = ""+error; });
+}

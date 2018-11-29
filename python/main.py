9979165d9c34d1c1e5d3869e5dfd43680bdd92e2
Swahgner <swahgnerdk@gmail.com>
2018-11-29 13:42:32 +0100

    Weather fix

--- python/main.py
+++ python/main.py
@@ -57,16 +57,16 @@ sense.show_message("Welcome to Berit", text_colour=(255,255,255), back_colour=(0
 while True:
 
   ## Temp from pi
-  temp1 = sense.get_temperature_from_humidity()
-  temp2 = sense.get_temperature_from_pressure()
-  temp_cpu = get_cpu_temperature()
+  pi_temp1 = sense.get_temperature_from_humidity()
+  pi_temp2 = sense.get_temperature_from_pressure()
+  pi_temp_cpu = get_cpu_temperature()
 
-  humidity = sense.get_humidity()
-  pressure = sense.get_pressure()
+  pi_humidity = sense.get_humidity()
+  pi_pressure = sense.get_pressure()
 
-  temp = (temp1+temp2)/2
-  temp_corr = temp - ((temp_cpu-temp)/1.5)
-  temp_corr = get_smooth(temp_corr)
+  pi_temp = (temp1+temp2)/2
+  pi_temp_corr = temp - ((temp_cpu-temp)/1.5)
+  pi_temp_corr = get_smooth(temp_corr)
   ## --------------
 
   msg = "Current temp in "
@@ -78,7 +78,7 @@ while True:
 
   for event in sense.stick.get_events():
       if (event.action == "pressed"):
-          sense.show_message(msg, text_colour=(255,255,255), back_colour=(0,0,0), scroll_speed=0.08)
+          sense.show_message(msg, text_colour=(255,255,255), back_colour=(0,0,0), scroll_speed=0.1)
           print(msg)
 
         #if (event.direction == "right"):

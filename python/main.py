62520b36e184eea9f92256efcaa94f625b9bdaf9
Swahgner <swahgnerdk@gmail.com>
2018-11-29 12:52:13 +0100

    Error

--- python/main.py
+++ python/main.py
@@ -23,7 +23,7 @@ def get_smooth(x):
 
 
 
-
+print("Started")
 
 
 while True:
@@ -44,7 +44,7 @@ while True:
 
   for event in sense.stick.get_events():
       if (event.action == "pressed"):
-          sense.show_message(msg, text_colour=(255,255,255), back_colour=(0,0,0), scroll_speed=0.08)
+          sense.show_message("Hello", text_colour=(255,255,255), back_colour=(0,0,0), scroll_speed=0.08)
 
         #if (event.direction == "right"):
         #  px = px + 1
